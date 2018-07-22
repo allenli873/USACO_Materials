3a5f6bb62db035119712138fec400068de3b77e0
Allen Li <allenli873@gmail.com>
2018-07-22 14:00:31 -0700

    Update cpp_template.h

--- Random Classes/cpp_template.h
+++ Random Classes/cpp_template.h
@@ -26,18 +26,24 @@ typedef vector<pi> vpi;
 template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
 
+template<class T> inline void MAX(T& a, T b) { if(a < b) a = b; }
+template<class T> inline void MIN(T& a, T b) { if(a > b) a = b; }
+template<class T> inline void MODA(T& a, T b) { a %= b; if(a < 0) a += b; }
+
 \#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(20)
 \#define watch(x) cout << (#x) << " is " << (x) << endl
 \#define long ll
 \#define finish(x) return cout << (x) << '\n', 0
-\#define F0R(i, a) for(int (i)=0;(i)<(a);(i)++)
-\#define FOR(i, a, b) for(int (i)=(a);(i)<(b);(i)++)
-\#define F0Rd(i, a) for(int (i)=a;(i)>=0;(i)--)
-\#define FORd(i, a, b) for(int (i)=a;(i)>=(b);(i)--)
+\#define F0R(i, a) for(int (i)=0;(i)<(a);++(i))
+\#define FOR(i, a, b) for(int (i)=(a);(i)<(b);++(i))
+\#define F0Rd(i, a) for(int (i)=a;(i)>=0;--(i))
+\#define FORd(i, a, b) for(int (i)=a;(i)>=(b);--(i))
 \#define all(x) (x).begin(), (x).end()
+\#define sz(x) ((int)(x).size())
 
 \#define mp make_pair
 \#define pb push_back
+\#define pf push_front
 \#define f first
 \#define s second
 \#define lb lower_bound
