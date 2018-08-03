deeda5b8dfd6590eedb1fd4e693f4ec796338988
Allen <allenli873@gmail.com>
2018-08-02 23:05:22 -0700

    Remodeled template

--- Random Classes/cpp_template.cpp
+++ Random Classes/cpp_template.cpp
@@ -1,58 +1,60 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <ctime>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cassert>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long double ld;
 typedef long long ll;
 typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<double,double> pd;
+typedef pair<ll, ll> pl;
+typedef pair<double, double> pd;
 
 typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<pi> vpi;
 
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template<class T, class U>
+inline void MAX(T &a, U b) { if (a < b) a = b; }
+
+template<class T, class U>
+inline void MIN(T &a, U b) { if (a > b) a = b; }
+
+template<class T, class U>
+inline void MODA(T &a, U b) {
+    a %= b;
+    if (a < 0) a += b;
+}
+
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(20)
 #define watch(x) cout << (#x) << " is " << (x) << endl
 #define long ll
-#define FAST ios::sync_with_stdio(0); cin.tie(0);
-#define finish(x) return cout << x << '\n', 0
-#define F0R(i, a) for(int i=0;i<a;i++)
-#define FOR(i, a, b) for(int i=a;i<b;i++)
-#define F0Rd(i, a) for(int i=a;i>=0;i--)
-#define FORd(i, a, b) for(int i=a;i>=b;i--)
-#define all(x) x.begin(), x.end()
+#define finish(x) return cout << (x) << '\n', 0
+#define F0R(i, a) for(int (i)=0;(i)<(a);++(i))
+#define FOR(i, a, b) for(int (i)=(a);(i)<(b);++(i))
+#define F0Rd(i, a) for(int (i)=a;(i)>=0;--(i))
+#define FORd(i, a, b) for(int (i)=a;(i)>=(b);--(i))
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
 
 #define mp make_pair
 #define pb push_back
-#define x first
-#define y second
+#define pf push_front
+#define f first
+#define s second
 #define lb lower_bound
 #define ub upper_bound
 
-int main() { FAST
+const int MOD = 1000000007;
+const int INF = 0x3f3f3f3f;
+const int NINF = -0x3f3f3f40;
+const ll INF_L = 0x3f3f3f3f3f3f3f3f;
+const ll NINF_L = -0x3f3f3f3f3f3f3f40;
 
+
+
+int main() {
+    FAST_IO;
+    
 }
+
