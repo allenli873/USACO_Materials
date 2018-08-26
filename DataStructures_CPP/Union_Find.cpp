ad5d923725e3b69dbf982240e4d5983f1fe9f3df
Allen Li <allenli873@gmail.com>
2018-08-26 14:56:38 -0700

    Update Union_Find.cpp

--- DataStructures_CPP/Union_Find.cpp
+++ DataStructures_CPP/Union_Find.cpp
@@ -1,19 +1,23 @@
-//make sure to initialize par (par[i]=i)
-int par[MAX_N];
-int ran[MAX_N];
+template<int SZ> struct DSU {
+	int par[SZ], ran[SZ];
 
-int find(int curr) {
-    return par[curr] == curr ? curr : (par[curr] = find(par[curr]));
-}
+	DSU() {
+		iota(par, par + SZ, 0);
+	}
+	
+	int find(int curr) {
+		return par[curr] == curr ? curr : (par[curr] = find(par[curr]));
+	}
 
-void unite(int n1, int n2) {
-    int f1 = find(n1);
-    int f2 = find(n2);
-    if(ran[f1] > ran[f2]) {
-        par[f2] = f1;
-        ran[f1]++;
-    } else {
-        par[f1] = f2;
-        ran[f2]++;
-    }
+	void unite(int n1, int n2) {
+		int f1 = find(n1);
+		int f2 = find(n2);
+		if(ran[f1] > ran[f2]) {
+			par[f2] = f1;
+			ran[f1]++;
+		} else {
+			par[f1] = f2;
+			ran[f2]++;
+		}
+	}
 }
