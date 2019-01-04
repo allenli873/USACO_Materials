df1c9d970bbc6e61a275ab797cd888ccfcb3ee71
Allen Li <allenli873@gmail.com>
2019-01-04 15:24:24 -0800

    Create Geo.cpp

---
+++ DataStructures/Comp Geo Cancer/Geo.cpp
@@ -0,0 +1,20 @@
+struct point {
+	int x, y;
+	point() { x = y = 0; }
+	point(int _x, int _y) : x(_x), y(_y) {}
+	bool operator < (point o) const {
+		if(x != o.x)
+			return x < o.x;
+		return y < o.y;
+	}
+};
+struct point_d {
+	double x, y;
+	point() { x = y = 0.0; }
+	point(double _x, double _y) : x(_x), y(_y) {}
+	bool operator < (point o) const {
+		if(abs(x-o.x) > EPS)
+			return x < o.x;
+		return y < o.y;
+	}
+};
