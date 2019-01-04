f9d7bfcb1a6062a73556ee56e072c86f095855c7
Allen Li <allenli873@gmail.com>
2019-01-04 15:25:48 -0800

    Update Geo.cpp

--- DataStructures/Comp Geo Cancer/Geo.cpp
+++ DataStructures/Comp Geo Cancer/Geo.cpp
@@ -10,10 +10,10 @@ struct point {
 };
 struct point_d {
 	double x, y;
-	point() { x = y = 0.0; }
-	point(double _x, double _y) : x(_x), y(_y) {}
+	point_d() { x = y = 0.0; }
+	point_d(double _x, double _y) : x(_x), y(_y) {}
 	bool operator < (point o) const {
-		if(abs(x-o.x) > EPS)
+		if(abs(x - o.x) > EPS)
 			return x < o.x;
 		return y < o.y;
 	}
