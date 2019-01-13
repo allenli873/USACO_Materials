c0f4351e18efcbf77081dca13f98cf8af1d29d82
Allen Li <allenli873@gmail.com>
2019-01-12 18:32:59 -0800

    Update mstree_online.cpp

--- DataStructures/Range Queries/mstree_online.cpp
+++ DataStructures/Range Queries/mstree_online.cpp
@@ -1,17 +1,17 @@
 template<int SZ> struct mstree {
-	ordered_set<pi> val[SZ+1];
+	ordered_set<int> val[SZ+1];
 
 	void update(int x, int y, int t = 1) {
 		for (int X = x; X <= SZ; X += X&-X) {
-			if (t == 1) val[X].insert({y,x});
-			else val[X].erase({y,x});
+			if (t == 1) val[X].insert(y);
+			else val[X].erase(y);
 		}
 	}
 
 	int query(int x, int y) {
 		int t = 0;
 		while(x > 0) {
-			t += val[x].order_of_key({y,INF});
+			t += val[x].order_of_key(y+1);
 			x -= x&-x;
 		}
 		return t;
