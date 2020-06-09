f9590975c0d793b62d8de1d6ec0c0cc2e2e1d347
Allen Li <allenli873@gmail.com>
2020-06-08 22:59:31 -0700

    Update cpp_template.cpp

--- Random Classes/cpp_template.cpp
+++ Random Classes/cpp_template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef long double ld;
 typedef long long ll;
+
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
 typedef pair<double, double> pd;
@@ -20,20 +21,35 @@ inline void MAX(T &a, U b) { if (a < b) a = b; }
 template<class T, class U>
 inline void MIN(T &a, U b) { if (a > b) a = b; }
 
+template<class T, class U, class V>
+inline bool INR(T &i, U &l, V &r) { return l <= i && i <= r; }
+
 template<class T, class U>
-inline void MODA(T &a, U b) {
+inline void MD(T &a, U b) {
     a %= b;
-    if (a < 0) a += b;
+    if(a < 0) a += b;
 }
 
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(20)
-#define watch(x) cout << (#x) << " is " << (x) << endl
-#define long ll
-#define finish(x) return cout << (x) << '\n', 0
+// DEBUG PRINTING TOOLS
+#define dbg(x) cout << (#x) << " is " << (x) << '\n'
+#define dbg2(x, y) cout << (#x) << " is " << (x) << " and " << (#y) << " is " << y << '\n'
+#define dbgarr(x, sz) for(int asdf = 0; asdf < (sz); asdf++) cout << x[asdf] << ' '; cout << '\n'
+#define dbgarr2(x, rose, colin) for(int asdf2 = 0; asdf2 < rose; asdf2++) { dbgarr(x[asdf2], colin); }
+#define dbgitem(x) for(auto asdf = x.begin(); asdf != x.end(); asdf++) cout << *asdf << ' '; cout << '\n'
+
+template <typename T> ostream& operator<<(ostream& os, const vector<T> &p){os << "["; for(int i = 0; i < p.size(); i++) os << p[i] << (i == p.size() - 1 ? "" : ",") ; os << "]"; return os;}
+template <typename T> ostream& operator<<(ostream& os, const set<T> &p){os << "{ "; for (T x: p) os << x << " "; os << "}"; return os;}
+template <typename T> ostream& operator<<(ostream& os, const multiset<T> &p){os << "{ "; for (T x: p) os << x << " "; os << "}"; return os;}
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U> &p){os << '{' << p.first << ',' << p.second << '}'; return os;}
+
+// WACKY FOR LOOPS
 #define F0R(i, a) for(int (i)=0;(i)<(a);++(i))
 #define FOR(i, a, b) for(int (i)=(a);(i)<(b);++(i))
 #define F0Rd(i, a) for(int (i)=a;(i)>=0;--(i))
 #define FORd(i, a, b) for(int (i)=a;(i)>=(b);--(i))
+
+// SIMPLIFICATION UTILITY
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(20)
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((int)(x).size())
 
@@ -45,16 +61,12 @@ inline void MODA(T &a, U b) {
 #define lb lower_bound
 #define ub upper_bound
 
-const int MOD = 1000000007;
+const int MOD = (int)(1e9) + 7;
 const int INF = 0x3f3f3f3f;
 const int NINF = -0x3f3f3f40;
 const ll INF_L = 0x3f3f3f3f3f3f3f3f;
 const ll NINF_L = -0x3f3f3f3f3f3f3f40;
 
-
-
 int main() {
     FAST_IO;
-    
 }
-
