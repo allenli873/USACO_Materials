4edb0b2b95baf190bc95ba3d03cfabdcc6f0700e
Allen Li <allenli873@gmail.com>
2020-06-14 10:36:31 -0700

    Create mi.cpp

---
+++ Copy_Pasteable/mi.cpp
@@ -0,0 +1,55 @@
+struct mi {
+    ll v; explicit operator ll() const { return v; }
+    mi() { v = 0; }
+    mi(ll _v) {
+        v = (-MOD <= _v && _v <= MOD) ? _v : _v % MOD;
+        if(v < 0) v += MOD;
+    }
+    friend bool operator==(const mi& a, const mi& b) { return a.v == b.v; }
+    friend bool operator!=(const mi& a, const mi& b) { return !(a == b); }
+    friend bool operator<(const mi& a, const mi& b) { return a.v < b.v; }
+    friend bool operator<=(const mi& a, const mi& b) { return a < b || a == b; }
+    friend bool operator>(const mi& a, const mi& b) { return a.v > b.v; }
+    friend bool operator>=(const mi& a, const mi& b) {return a > b || a == b; }
+    mi operator-() const { return mi(-v); }
+    mi& operator+=(const mi& m) {
+        if((v += m.v >= MOD)) v -= MOD;
+        return *this;
+    }
+    mi& operator-=(const mi& m) {
+        if(v -= m.v < 0) v += MOD;
+        return *this;
+    }
+    mi& operator*=(const mi& m) {
+        v = v * m.v % MOD;
+        return *this;
+    }
+    mi& operator/=(const mi& m) {
+        return (*this) *= inv(m);
+    }
+    mi& operator++() { return *this += 1; }
+    mi& operator--() { return *this -= 1; }
+    friend mi operator+(mi a, const mi& b) { return a += b; }
+    friend mi operator-(mi a, const mi& b) { return a -= b; }
+    friend mi operator*(mi a, const mi& b) { return a *= b; }
+    friend mi operator/(mi a, const mi& b) { return a /= b; }
+    friend mi pow(mi b, ll p) {
+        assert(p >= 0);
+        mi ret = 1; 
+        for(; p; p /= 2, b *= b) if(p & 1) ret *= b;
+        return ret;
+    }
+    friend mi inv(const mi& a) {
+        assert(a.v != 0);
+        return pow(a, MOD - 2);
+    }
+    // printing
+    friend ostream& operator<<(ostream& os, const mi& m) {
+        os << m.v; return os;
+    }
+    friend istream& operator>>(istream& is, mi& m) {
+        ll x; is >> x;
+        m.v = x;
+        return is;
+    }
+};
