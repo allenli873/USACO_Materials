794896da20b284cd1eae960efaf81e1aca31ab81
Allen Li <allenli873@gmail.com>
2021-01-02 11:25:31 -0800

    Update matmul.cpp
    verification: https://codeforces.com/contest/186/submission/103059342

--- Copy_Pasteable/matmul.cpp
+++ Copy_Pasteable/matmul.cpp
@@ -1,85 +1,63 @@
-struct mi {
-    ll v; explicit operator ll() const { return v; }
-    mi() { v = 0; }
-    mi(ll _v) {
-        v = (-MOD <= _v && _v <= MOD) ? _v : _v % MOD;
-        if(v < 0) v += MOD;
+struct Mat {
+    Mat(int N, int M) {
+        m.resize(N);
+        for (vector<T> &v : m)
+            v.resize(M);
     }
-    friend bool operator==(const mi& a, const mi& b) { return a.v == b.v; }
-    friend bool operator!=(const mi& a, const mi& b) { return !(a == b); }
-    friend bool operator<(const mi& a, const mi& b) { return a.v < b.v; }
-    friend bool operator<=(const mi& a, const mi& b) { return a < b || a == b; }
-    friend bool operator>(const mi& a, const mi& b) { return a.v > b.v; }
-    friend bool operator>=(const mi& a, const mi& b) {return a > b || a == b; }
-    mi operator-() const { return mi(-v); }
-    mi& operator+=(const mi& m) {
-        if((v += m.v) >= MOD) v -= MOD;
-        return *this;
-    }
-    mi& operator-=(const mi& m) {
-        if((v -= m.v) < 0) v += MOD;
-        return *this;
-    }
-    mi& operator*=(const mi& m) {
-        v = v * m.v % MOD;
-        return *this;
-    }
-    mi& operator/=(const mi& m) {
-        return (*this) *= inv(m);
-    }
-    mi& operator++() { return *this += 1; }
-    mi& operator--() { return *this -= 1; }
-    friend mi operator+(mi a, const mi& b) { return a += b; }
-    friend mi operator-(mi a, const mi& b) { return a -= b; }
-    friend mi operator*(mi a, const mi& b) { return a *= b; }
-    friend mi operator/(mi a, const mi& b) { return a /= b; }
-    friend mi pow(mi b, ll p) {
-        assert(p >= 0);
-        mi ret = 1;
-        for(; p; p /= 2, b *= b) if(p & 1) ret *= b;
-        return ret;
-    }
-    friend mi inv(const mi& a) {
-        assert(a.v != 0);
-        return pow(a, MOD - 2);
-    }
-    // printing
-    friend ostream& operator<<(ostream& os, const mi& m) {
-        os << m.v; return os;
-    }
-    friend istream& operator>>(istream& is, mi& m) {
-        ll x; is >> x;
-        m.v = x;
-        return is;
-    }
-};
+    Mat(vector<vector<T>> &vec) : m(vec) {}
+    Mat(Mat<T> &mat) : m(mat.m) {}
 
-vector<vector<mi>> matmul(vector<vector<mi>>& mat1, vector<vector<mi>>& mat2) {
-    int n1 = sz(mat1), m1 = sz(mat1[0]), n2 = sz(mat2), m2 = sz(mat2[0]);
-    assert(m1 == n2);
-    vector<vector<mi>> res(n1, vector<mi>(m2, 0));
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m2; j++) {
-            for (int a = 0; a < m1; a++) {
-                res[i][j] += mat1[i][a] * mat2[a][j];
+    vector<T> &operator[](int idx) { return m[idx]; }
+    const vector<T> &operator[](int idx) const { return m[idx]; }
+    int rows() const { return m.size(); }
+    int cols() const { return m[0].size(); }
+
+    static void exp(Mat &mat, ll po) {
+        assert(mat.rows() == mat.cols());
+        int n = mat.rows();
+        Mat<T> a(mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) mat[i][j] = 1;
+                else mat[i][j] = 0;
             }
         }
+        while (po) {
+            if (po & 1)
+                mat = mult(mat, a);
+            a = mult(a, a);
+            po >>= 1;
+        }
     }
-    return res;
-}
 
-void expo(vector<vector<mi>>& mat, ll exp) {
-    assert(sz(mat) == sz(mat[0]));
-    int n = sz(mat);
-    vector<vector<mi>> a(mat);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            mat[i][j] = 0;
-    for (int i = 0; i < n; i++) mat[i][i] = 1;
-    while (exp) {
-        if (exp & 1)
-            mat = matmul(mat, a);
-        a = matmul(a, a);
-        exp >>= 1;
+    static Mat mult(const Mat &mat1, const Mat &mat2) {
+        int n1 = mat1.rows(), m1 = mat1.cols();
+        int n2 = mat2.rows(), m2 = mat2.cols();
+        assert(m1 == n2);
+        Mat res(n1, m2);
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m2; j++) {
+                for (int a = 0; a < m1; a++) {
+                    res[i][j] += mat1[i][a] * mat2[a][j];
+                }
+            }
+        }
+        return res;
     }
-}
+
+    //printing
+    friend ostream &operator<<(ostream &os, const Mat &m) {
+        for (int i = 0; i < m.rows(); i++) {
+            os << "[";
+            for (int j = 0; j < m.cols(); j++) {
+                os << m[i][j];
+                if (j != m.cols() - 1) os << ',';
+            }
+            os << "]";
+            if (i != m.rows() - 1) os << '\n';
+        }
+        return os;
+    }
+    
+    vector<vector<T>> m;
+};
