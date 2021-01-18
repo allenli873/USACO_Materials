9bc21ebf683eebf2671881ddf0eb907e9be5cca0
Allen Li <allenli873@gmail.com>
2021-01-18 15:24:19 -0800

    Update os.cpp

--- Copy_Pasteable/os.cpp
+++ Copy_Pasteable/os.cpp
@@ -4,4 +4,6 @@
 using namespace __gnu_pbds;
 
 // IMPORTANT: less_equal for multiset functionality
+// also, when using less_equal, use os.erase(os.upper_bound(num)) to erase the number
+// find seems broken and lower bound and upper bound are switched with less_equal
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
